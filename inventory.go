b98aacf47cda44cf9e6d935b12809fdd4daacde9
aerth <aerth@riseup.net>
2017-06-22 06:57:23 +0000

    scroll inventory

--- inventory.go
+++ inventory.go
@@ -2,6 +2,7 @@ package rpg
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/imdraw"
@@ -10,10 +11,9 @@ import (
 )
 
 func InventoryLoop(win *pixelgl.Window, world *World) {
-	x, y := win.Bounds().Center().XY()
-	text := NewTextSmooth(14)
-	text.WriteString("\tGAME PAUSED\n\n")
-	text.WriteString("\tESC or any key to return\n\n")
+	text := NewText(28)
+	text.WriteString("\tGAME PAUSED\n")
+	text.WriteString("\tESC or any key to return\n\tUse PGUP/PGDOWN to scroll\n\n")
 	text.WriteString(world.Char.Stats.String())
 	text.WriteString(fmt.Sprintf("Level %v\nHealth: %v\nMana: %v\nXP: %v/%v", world.Char.Level, world.Char.Health, world.Char.Mana, world.Char.Stats.XP, world.Char.NextLevel()))
 	for _, item := range world.Char.Inventory {
@@ -24,33 +24,49 @@ func InventoryLoop(win *pixelgl.Window, world *World) {
 	}
 
 	text.WriteString("\n\n===INVENTORY===\n" + FormatItemList(world.Char.Inventory))
+	var page = 500.00
 	for !win.Closed() {
 
+		// controls
+		switch {
+		default:
+		case win.JustPressed(pixelgl.KeyPageUp) || win.JustPressed(pixelgl.KeyUp):
+			page -= 100
+			log.Println(page)
+			if page < 500 {
+				page = 500
+			}
+		case win.JustPressed(pixelgl.KeyPageDown) || win.JustPressed(pixelgl.KeyDown):
+			page += 100
+
+			log.Println(page)
+		}
+
 		win.Clear(colornames.Black)
-		x++
-		y++
-		if x > win.Bounds().Max.X {
-			x = 0
-		}
-		if y > win.Bounds().Max.Y {
-			y = 0
-		}
 		imd := imdraw.New(nil)
 		imd.Color = colornames.Green
 		imd.Push(pixel.V(0, 0), win.Bounds().Max)
-		imd.Rectangle(4)
-		imd.Color = colornames.White
-		imd.Push(pixel.V(x+100, y+100), pixel.V(x+110, y+110))
-		imd.Rectangle(0)
-		imd.Push(pixel.V(x, y), pixel.V(x+10, y+10))
-		imd.Rectangle(0)
+		imd.Rectangle(30)
+		imd.Color = colornames.Yellow
+		imd.Push(pixel.V(0, 0), win.Bounds().Max)
+		imd.Rectangle(20)
+		imd.Color = colornames.Red
+		imd.Push(pixel.V(0, 0), win.Bounds().Max)
+		imd.Rectangle(10)
 		imd.Draw(win)
 
-		if win.Typed() != "" || win.JustPressed(pixelgl.KeyEscape) {
+		// break loop
+		if win.Typed() != "" || win.JustPressed(pixelgl.KeyEscape) || win.JustPressed(pixelgl.KeyEnter) {
 			break
 		}
-		text.Draw(win, pixel.IM.Moved(pixel.V(30, 500)))
-		win.SetTitle("AERPG inventory")
+
+		// draw text
+		text.Draw(win, pixel.IM.Moved(pixel.V(30, page)))
+
+		// update window
 		win.Update()
+
+		// set title
+		win.SetTitle("AERPG inventory")
 	}
 }
